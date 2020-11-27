3f9b841be7ed255425178216b274e575fc41d330
chilochibi <42684753+chilochibi@users.noreply.github.com>
2020-11-27 16:20:13 +0200

    no message

--- Nigeria/Research/urban_rural_cluster_analysis_cc/data_extraction_imputation.R
+++ Nigeria/Research/urban_rural_cluster_analysis_cc/data_extraction_imputation.R
@@ -39,57 +39,102 @@ dhs2 <- sapply(dhs2, read_dta, simplify = F)
 
 pfpr_data <- dhs[[1]] # uses the DHS person recode dataset 
 
-look_for(dhs[[1]], "smear")
+#merging with the KR dataset
 
-table(pfpr_data$hml32) # frequency table for smear test 
+pfpr_care <- dhs2[[1]] %>% filter(b5 == 1  & b19 <  60 & h22 == 1) 
 
-#subsetting for microscopy (denominator -hh selected for hemoglobin, child slept there last night and have result for test)
+pfpr_care <- pfpr_care[,c("v001", "ml13e")]
+colnames(pfpr_care)[1]<- "hv001"
 
-pfpr_df <- pfpr_data %>% filter(hv042 == 1 & hv103 == 1 & hc1 %in% c(6:59) & hml32 %in% c(0, 1,6))
+#pfpr_data <- left_join(pfpr_data, pfpr_care, by = "hv001") 
 
-
-
-
-
-# prep dataset for cluster level analysis - we start with urban cluster analysis 
-
-val_labels(pfpr_df$hv025) # value labels for place of residence, 1 = urban and 2 = rural
-
-#pfpr_place <- pfpr_df %>% filter(hv025 == 1)
-#pfpr_dhs <- pfpr_data%>% filter(hv025 == 1)
-pfpr_place <- pfpr_df
-pfpr_dhs <- pfpr_data
-# estimate cluster-level malaria prevalence
-
-pfpr_place<- funEnv$dataclean.para(pfpr_place, hv005, hc1, hml32, 'hml32', 'p_test') 
+pfpr_data2 <- pfpr_data
 
 
-###############P_TEST_IMPUTATION##############
-# create factor variables 
-pfpr_place_imp <- pfpr_place[,c("hv001", 'p_test')]
-pfpr_place_imp$p_test <- factor(pfpr_place_imp$p_test)
+# Treat variables as factors
+pfpr_data2$hml32 <- as.factor(pfpr_data2$hml32) #p_test
+pfpr_data2$hv270 <- as.factor(pfpr_data2$hv270) #wealth_2
+pfpr_data2$hml16 <- as.factor(pfpr_data2$hml16) #age
+pfpr_data2$ha54 <- as.factor(pfpr_data2$ha54) #preg
+pfpr_data2$hv106 <- as.factor(pfpr_data2$hv106) #education
+pfpr_data2$hv103 <- as.factor(pfpr_data2$hv103) #ITN use proportion among u5
+pfpr_data2$hv104 <- as.factor(pfpr_data2$hv104) #ITN use proportion among pregnant women 
+pfpr_data2$hv009 <- as.factor(pfpr_data2$hv009) # median household size
 
-# obtain information about missing data 
-inf <-mi.info(pfpr_place_imp) #not working
+p_missing <- unlist(lapply(pfpr_data2, function(x) sum(is.na(x))))/nrow(pfpr_data)
+sort(p_missing[p_missing > 0], decreasing = TRUE)
 
-imp <- mice(pfpr_place_imp,n.imp=5,seed=1934)
+# Select variables that could cause problems in the imputation process
+pfpr_data2 <- pfpr_data2 %>% 
+  dplyr::select(hv001, hml32, hv270, hml16, ha54, hv106, hv103, hv104, hv009)
 
-# convert mids to data useable for work in mitools
-imp <- as.numeric(imp)
-mydata <- imputationList(lapply(1:5, complete, x=imp))
-summary(mydata) 
+# We run the mice code with 0 iterations 
 
-# next we estimate proportion of people in high SES by cluster
-# recode the weath quintile variable 
-table(pfpr_df$hv270)
+imp <- mice(pfpr_data2, maxit=0)
 
-pfpr_wealth <- pfpr_df %>%  mutate(wealth = ifelse(hv270 <4, 0, 1))
-table(pfpr_wealth$wealth)
+# Extract predictorMatrix and methods of imputation 
 
-pfpr_wealth<- funEnv$dataclean.para(pfpr_wealth, hv005, hv005, wealth, 'wealth', 'wealth_2') 
-table(pfpr_wealth$wealth_2)
+predM <- imp$predictorMatrix
+meth <- imp$method
+
+# Setting values of variables I'd like to leave out to 0 in the predictor matrix
+predM[, c("hv001")] <- 0
+
+# Specify a separate imputation model for variables of interest 
+
+# Ordered categorical variables 
+poly <- c("hv270", "hv106")
+
+# Dichotomous variable
+log <- c("hml32", "ha54", "hv103","hv104","hv009")
+
+# Turn their methods matrix into the specified imputation models
+meth[poly] <- "polr"
+meth[log] <- "logreg"
+
+# With this command, we tell mice to impute the pfpr_data2 data, create 5
+# datasets, use predM as the predictor matrix and don't print the imputation
+# process. If you would like to see the process, set print as TRUE
+
+imp2 <- mice(pfpr_data2, maxit = 5, 
+             predictorMatrix = predM, 
+             method = meth, print =  FALSE)
+
+head(imp2$imp$hml32)
+tail(imp2$imp$hml32)
+
+# extraction of the first imputed, complete dataset and look at the first
+# rows using the complete function
+
+pfpr_data2comp <- mice::complete(imp2, 1)
+head(pfpr_data2comp)
 
 
+# First, turn the datasets into long format
+pfpr_dataimp_long <- mice::complete(imp2, action="long", include = TRUE)
 
 
+# Convert two variables into numeric
+pfpr_dataimp_long$hml16 <- with(pfpr_dataimp_long, as.integer(pfpr_dataimp_long$hml16))
 
+pfpr_dataimp_long$hv009 <- with(pfpr_dataimp_long, as.integer(pfpr_dataimp_long$hv009))
+
+
+#Convert back to mids type - mice can work with this type
+pfpr_dataimp_long_mids<-as.mids(pfpr_dataimp_long)
+
+
+#Replace inputed coloums
+
+replace_imputed <- function(pfpr_data, pfpr_dataimp_long_mids){
+  
+  namestoChange <- colnames(pfpr_data)[colnames(pfpr_dataimp_long_mids) %in% colnames(pfpr_data)]
+  
+  for(i in 1:length(namestoChange)){
+    pfpr_data[namestoChange[i]] <- pfpr_dataimp_long_mids[namestoChange[i]]
+  }
+  return(pfpr_data)
+  
+}
+
+pfpr_data_inputed <- replace_imputed(pfpr_data, pfpr_dataimp_long_mids)
